25e9edd011112e6d516c4391de2194eae51a5d95
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-13 13:14:39 -0400

    style header with flexbox

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,30 @@
+import Link from "next/link"
+import Image from "next/image"
+
 function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>  
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+      <div className="flex items-center space-x-5">
+          <Link href="/">
+            <Image 
+              className="cursor-pointer object-contain"
+              src="/medium.png" 
+              alt="" 
+              width="60px"
+              height="60px" 
+            />
+          </Link>
+      </div>
+      <div className="hidden md:inline-flex items-center space-x-5">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">Follow</h3>
+      </div>
+      <div className="flex items-center space-x-5 text-green-600">
+          <h3>Sign in</h3>
+          <h3 className="border px-4 py-1 rounded-full border-green-600">Get Started</h3>
+      </div>
+    </header>  
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,11 @@ import Header from "../components/Header"
 
 const Home = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Medium Clone</title>
       </Head>
-      
+
      <Header />
     </div>
   )
