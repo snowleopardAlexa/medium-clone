72ec16f0f93b5a52ac16c4b5797883e16dcaca7f
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-13 17:53:34 -0400

    add comment form

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -28,12 +28,12 @@ const Post = ({ post } : Props ) => {
              src={urlFor(post.author.image).url()!}
              alt="author-image"
            />
-           <p className="font-extralight text-sm">Blog post by 
-           <span className="text-green-600">{post.author.name}</span>
+           <p className="font-extralight text-sm">Blog post by  
+            <span className="text-green-600"> {post.author.name} </span>
            - Published at {new Date(post._createdAt).toLocaleString()}
            </p>
        </div>
-       <div>
+       <div className="mt-10">
            <PortableText 
              className=""
              dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
@@ -44,7 +44,7 @@ const Post = ({ post } : Props ) => {
                      <h1 className="text-2xl font-bold my-5" {...props} />
                  ),
                  h2: (props: any) => (
-                     <li className="ml-4 list-disc" {...props} />
+                     <li className="ml-4 list-disc my-5" {...props} />
                  ),
                  li: ({ children }: any) => (
                     <li className="ml-4 list-disc">{children}</li>
@@ -58,6 +58,25 @@ const Post = ({ post } : Props ) => {
            />
        </div>
       </article>
+      <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
+      
+      <form className="flex flex-col p-5 my-10 max-w-2xl mx-auto mb-10">
+          <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+          <h4 className="text-3xl font-bold">Leave a commment below!</h4>
+          <hr className="py-3 mt-2" />
+          <label className="block mb-5">
+              <span className="text-grey-700">Name</span>
+              <input className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500" placeholder="Name" type="text" />
+          </label>
+          <label className="block mb-5">
+              <span>Email</span>
+              <input className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500" placeholder="Email" type="text" />
+          </label>
+          <label className="block mb-5">
+              <span>Comment</span>
+              <textarea className="shadow border rounded py-2 px-3 form-textarea mr-1 w-full ring-yellow-500 outline-none focus:ring" placeholder="Type a comment..." rows={8} />
+          </label>
+      </form>
     </main>
   )
 }
