4770590b594310a652c7134d4edb9bf5e645623d
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-13 14:33:10 -0400

    add interface definition typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import Head from 'next/head'
 import Header from "../components/Header"
 import Image from "next/image"
+import { sanityClient, urlFor } from "../sanity"
 
-const Home = () => {
+interface Props {
+  posts: Post[]
+}
+
+const Home = (props: Props) => {
   return (
     <div className=" max-w-7xl mx-auto">
       <Head>
@@ -32,3 +37,27 @@ const Home = () => {
 }
 
 export default Home
+
+// enable server side rendering in next.js
+export const getServerSideProps = async() => {
+  const query = `
+  *[_type == "post"]{
+    _id,
+    title,
+    author => {
+    name,
+    image
+    },
+    description,
+    mainImage,
+    slug
+  }
+  `
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts,
+    }
+  }
+}
