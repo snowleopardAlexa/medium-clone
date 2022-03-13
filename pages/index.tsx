76492c4366b3fb82b2f883d1139da852158b97c5
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-13 16:46:35 -0400

    create post

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import Head from 'next/head'
 import Header from "../components/Header"
 import Image from "next/image"
+import Link from 'next/link'
 import { sanityClient, urlFor } from "../sanity"
+import { Post } from "../typings"
 
 interface Props {
   posts: Post[]
 }
 
-const Home = (props: Props) => {
+const Home = ({ posts }: Props) => {
+  console.log(posts)
   return (
     <div className=" max-w-7xl mx-auto">
       <Head>
@@ -32,6 +35,34 @@ const Home = (props: Props) => {
          height="200px"
        />
      </div>
+     {/* Posts */}
+     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+       {posts.map(post => (
+         <Link key={post._id} href={`/post/${post.slug.current}`}>
+           <div className="group cursor-pointer border rounded-large overflow-hidden">
+             <img
+              className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out" 
+              src={urlFor(post.mainImage).url()!}
+              alt="post-image"
+              width="100%"
+              height="100%"
+             />
+             <div className="flex justify-between p-5 bg-white">
+               <div>
+                 <p className="text-lg font-bold">{post.title}</p>
+                 <p>{post.description} by {post.author.name}</p>
+               </div>
+               <div>
+                 <img 
+                   className="h-12 w-12 rounded-full"
+                   src={urlFor(post.author.image).url()!} alt="author-image" 
+                  />
+               </div>
+             </div>
+           </div>
+         </Link>
+       ))}
+     </div>
     </div>
   )
 }
@@ -40,13 +71,13 @@ export default Home
 
 // enable server side rendering in next.js
 export const getServerSideProps = async() => {
-  const query = `
-  *[_type == "post"]{
+  const query = 
+  ` *[_type == "post"]{
     _id,
     title,
-    author => {
-    name,
-    image
+    author-> {
+     name,
+     image
     },
     description,
     mainImage,
---
+++ pages/post/[slug].tsx
@@ -0,0 +1,106 @@
+import { GetStaticProps } from "next"
+import Header from "../../components/Header"
+import { sanityClient, urlFor } from "../../sanity"
+import { Post } from "../../typings"
+
+interface Props {
+  post: Post;
+}
+
+const Post = ({ post } : Props ) => {
+  return (
+    <main>
+      <Header />
+      <img 
+       className="w-full h-40 object-cover"
+       src={urlFor(post.mainImage).url()!}
+       alt="post-image-cover"
+      />
+      <article className="max-w-3xl mx-auto p-5">
+       <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+       <h2 className="text-xl font-light text-gray-500 mb-2">
+           {post.description}
+       </h2>
+       <div className="flex items-center space-x-2">
+           <img
+             className="h-10 w-10 rounded-full" 
+             src={urlFor(post.author.image).url()!}
+             alt="author-image"
+           />
+           <p className="font-extralight text-sm">Blog post by 
+           <span className="text-green-600">{post.author.name}</span>
+           - Published at {new Date(post._createdAt).toLocaleString()}
+           </p>
+       </div>
+      </article>
+    </main>
+  )
+}
+
+export default Post
+
+export const getStaticPaths = async() => {
+  const query = 
+  ` *[_type == "post"]{
+    _id,
+    slug  {
+    current
+   }
+  }`
+
+  const posts = await sanityClient.fetch(query)
+
+  const paths = posts.map((post: Post) => ({
+      params: {
+          slug: post.slug.current
+      }
+  }))
+
+  return {
+      paths,
+      fallback: 'blocking',
+  }
+}
+
+// get the post from sanity
+export const getStaticProps: GetStaticProps = async({ params }) => {
+  const query = 
+  `
+    *[_type == "post" && slug.current == $slug][0] {
+        _id,
+        _createdAt,
+        title,
+        author-> {
+            name,
+            image
+        },
+        'comments': *[
+            _type == "comment" &&
+            post._ref == ^._id &&
+            approved == true],
+        description,
+        mainImage,
+        slug,
+        body
+    }`
+  
+
+   const post = await sanityClient.fetch(query, {
+       slug: params?.slug,
+   })
+
+   // no post to come back from fetch
+   if (!post) {
+       return {
+           notFound: true
+       }
+   }
+
+   return {
+       props: {
+           post,
+       },
+       revalidate: 60, // after 60 sec revalidates/updates the old cached version
+   }
+}
+
