e0fe8f60bfcb71db0dff6335c2bca02f6fa2aff7
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-13 13:42:33 -0400

    add medium logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,19 +7,20 @@ function Header() {
       <div className="flex items-center space-x-5">
           <Link href="/">
             <Image 
-              className="cursor-pointer object-contain"
+              className="cursor-pointer w-44 object-contain"
               src="/medium.png" 
               alt="" 
-              width="60px"
-              height="60px" 
+              width="55px"
+              height="55px" 
             />
           </Link>
-      </div>
       <div className="hidden md:inline-flex items-center space-x-5">
           <h3>About</h3>
           <h3>Contact</h3>
           <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">Follow</h3>
       </div>
+      </div>
+
       <div className="flex items-center space-x-5 text-green-600">
           <h3>Sign in</h3>
           <h3 className="border px-4 py-1 rounded-full border-green-600">Get Started</h3>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,32 @@
 import Head from 'next/head'
 import Header from "../components/Header"
+import Image from "next/image"
 
 const Home = () => {
   return (
-    <div className="">
+    <div className=" max-w-7xl mx-auto">
       <Head>
         <title>Medium Clone</title>
       </Head>
 
      <Header />
+     <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10">
+       <div className="px-10 space-y-5">
+         <h1 className="text-6xl max-w-xl font-serif">
+           <span className="underline decoration-black decoration-4">Medium</span>{" "}
+           is a place to write, read, and connect</h1>
+         <h2>It's easy and free to post your thinking on any topic and connect
+           with millions of readers. 
+         </h2>
+       </div>
+       <Image 
+         className="hidden md:inline-flex h-32 lg:h-full"
+         src="/medium-letter.png"
+         alt="medium-logo"
+         width="270px"
+         height="200px"
+       />
+     </div>
     </div>
   )
 }
