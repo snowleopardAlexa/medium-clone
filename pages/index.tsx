6683c6476fbfabb6e63261d19bbf5f5142181be8
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-13 12:44:19 -0400

    add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from 'next/head'
+import Header from "../components/Header"
 
 const Home = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
         <title>Medium Clone</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Medium Clone</h1>
+      
+     <Header />
     </div>
   )
 }
